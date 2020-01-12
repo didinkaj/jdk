0b6677c57da20f6f9c9e705a7226ceeeb226b835
Johnson Didinya <didinkaj@gmail.com>
2020-01-12 17:29:37 +0300

    suppressing lint hightlight

--- app/src/main/java/com/example/jdk/MainActivity.kt
+++ app/src/main/java/com/example/jdk/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.jdk
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.webkit.WebView
 import android.webkit.WebViewClient
@@ -8,6 +9,7 @@ import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
